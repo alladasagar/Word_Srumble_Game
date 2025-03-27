06963c7a39ffed3e994ca8157b037bcb41961865
alladasagar <sagarallada258@gmail.com>
2025-03-27 21:30:14 +0530

    back to change

--- client/src/Pages/Game.jsx
+++ client/src/Pages/Game.jsx
@@ -56,6 +56,7 @@ const Game = () => {
     if (gameOver && user) {
       setLoading(true);
       sendScore({ email: user.email, score }).finally(() => setLoading(false));
+      console.log("Sending Score:", { email: user?.email, score });
     }
   }, [gameOver, user, score]);
 
