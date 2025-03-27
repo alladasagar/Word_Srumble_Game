7f0978855d9ce09c1f09db73caa576f43ba1f7f5
alladasagar <sagarallada258@gmail.com>
2025-03-27 23:10:28 +0530

    sign up

--- client/src/Pages/SignUpForm.jsx
+++ client/src/Pages/SignUpForm.jsx
@@ -167,7 +167,7 @@ export default function SignUpForm() {
                             disabled={loading}
                         >
                             <img src="https://www.google.com/favicon.ico" alt="Google" className="w-5 h-5 mr-2" />
-                            Sign in with Google
+                            Sign up with Google
                         </button>
 
                         <button
